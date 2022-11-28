ce90bd2174f08ed4fd22e475950deb8a0ff6e292
gabloa <gl2480@columbia.edu>
2022-11-28 15:40:06 -0500

    single_main to main

--- main.py
+++ main.py
@@ -4,44 +4,33 @@ import argparse
 import pprint
 import torch
 
-from config import get_configs, load_configs, parse_config_arg
+from config import get_single_config, load_config, parse_config_arg
 from two_step_zoo import (
-    get_two_step_module, get_trainer, get_loaders_from_config,
+    get_single_module, get_single_trainer, get_loaders_from_config,
     get_writer, get_evaluator, get_ood_evaluator
 )
 
 
-parser = argparse.ArgumentParser(description="Two Step Manifold Density Estimator")
+parser = argparse.ArgumentParser(
+    description="Single Density Estimation or Generalized Autoencoder Training Module"
+)
 
 parser.add_argument("--dataset", type=str,
-    help="Dataset to train on. Required if load-dir not specified.")
-parser.add_argument("--gae-model", type=str,
-    help="Model for generalized autoencoding. Required if load-dir not specified.")
-parser.add_argument("--de-model", type=str,
-    help="Model for density estimation. Required if load-dir not specified.")
+    help="Dataset to train on. Required if load_dir not specified.")
+parser.add_argument("--model", type=str,
+    help="Model to train. Required if load_dir not specified.")
+parser.add_argument("--is-gae", action="store_true",
+    help="Indicates that we are training a generalized autoencoder.")
 
 parser.add_argument("--load-dir", type=str, default="",
     help="Directory to load from.")
-parser.add_argument("--load-best-valid-first", action="store_true",
-    help="Attempt to load the best_valid checkpoint first.")
-parser.add_argument("--load-pretrained-gae", action="store_true",
-    help="Load only pretrained gae from resume-dir.")
-parser.add_argument("--freeze-pretrained-gae", action="store_true",
-    help="Freeze the parameters of the pretrained GAE, i.e. do not train them.")
-
 parser.add_argument("--max-epochs-loaded", type=int,
     help="New maximum shared epochs for loaded model.")
-parser.add_argument("--max-epochs-loaded-gae", type=int,
-    help="New maximum epochs for loaded GAE model.")
-parser.add_argument("--max-epochs-loaded-de", type=int,
-    help="New maximum epochs for loaded DE model.")
+parser.add_argument("--load-best-valid-first", action="store_true",
+    help="Load the best_valid checkpoint first")
 
-parser.add_argument("--gae-config", default=[], action="append",
-    help="Override gae config entries. Specify as `key=value`.")
-parser.add_argument("--de-config", default=[], action="append",
-    help="Override de config entries. Specify as `key=value`.")
-parser.add_argument("--shared-config", default=[], action="append",
-    help="Override shared config entries. Specify as `key=value`.")
+parser.add_argument("--config", default=[], action="append",
+    help="Override config entries. Specify as `key=value`.")
 
 parser.add_argument("--only-test", action="store_true",
     help="Only perform a test, no training.")
@@ -56,95 +45,81 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 if args.load_dir:
-    gae_cfg, de_cfg, shared_cfg = load_configs(
-        args=args,
-        density_estimator=args.de_model if args.de_model else None
+    # NOTE: Not updating config values using cmd line arguments (besides max_epochs)
+    #       when loading a run.
+    cfg = load_config(
+        args=args
     )
-
-    if args.load_pretrained_gae:
-        # NOTE: When loading a pretrained GAE, we do not expect to load either de_cfg or shared_cfg
-        de_cfg = {**de_cfg, **dict(parse_config_arg(kv) for kv in args.de_config)}
-        shared_cfg = {**shared_cfg, **dict(parse_config_arg(kv) for kv in args.shared_config)}
-
 else:
-    gae_cfg, de_cfg, shared_cfg = get_configs(
+    cfg = get_single_config(
         dataset=args.dataset,
-        generalized_autoencoder=args.gae_model,
-        density_estimator=args.de_model
+        model=args.model,
+        gae=args.is_gae,
+        standalone=True
     )
-    gae_cfg = {**gae_cfg, **dict(parse_config_arg(kv) for kv in args.gae_config)}
-    de_cfg = {**de_cfg, **dict(parse_config_arg(kv) for kv in args.de_config)}
-    de_cfg["data_dim"] = gae_cfg["latent_dim"]
-    shared_cfg = {**shared_cfg, **dict(parse_config_arg(kv) for kv in args.shared_config)}
+    cfg = {**cfg, **dict(parse_config_arg(kv) for kv in args.config)}
 
 
 pprint.sorted = lambda x, key=None: x
 pp = pprint.PrettyPrinter(indent=4)
-print(10*"-" + "-gae_cfg--" + 10*"-")
-pp.pprint(gae_cfg)
-print(10*"-" + "--de_cfg--" + 10*"-")
-pp.pprint(de_cfg)
-print(10*"-" + "shared_cfg" + 10*"-")
-pp.pprint(shared_cfg)
+print(10*"-" + "cfg" + 10*"-")
+pp.pprint(cfg)
 
 
-train_loader, valid_loader, test_loader = get_loaders_from_config(shared_cfg)
-writer = get_writer(args, gae_cfg=gae_cfg, de_cfg=de_cfg, shared_cfg=shared_cfg)
-two_step_module = get_two_step_module(gae_cfg, de_cfg, shared_cfg).to(device)
+train_loader, valid_loader, test_loader = get_loaders_from_config(cfg)
+writer = get_writer(args, cfg=cfg)
 
 
-gae_evaluator = get_evaluator(
-    two_step_module.generalized_autoencoder,
-    valid_loader=valid_loader, test_loader=test_loader,
-    train_loader=train_loader,
-    valid_metrics=gae_cfg["valid_metrics"],
-    test_metrics=gae_cfg["test_metrics"],
-    **gae_cfg.get("metric_kwargs", {}),
-)
-de_evaluator = get_evaluator(
-    two_step_module.density_estimator,
-    valid_loader=None, test_loader=None, # Loaders must be updated later by the trainer
-    train_loader=train_loader,
-    valid_metrics=de_cfg["valid_metrics"],
-    test_metrics=de_cfg["test_metrics"],
-    **de_cfg.get("metric_kwargs", {}),
-)
+module = get_single_module(
+    cfg,
+    data_dim=cfg["data_dim"],
+    data_shape=cfg["data_shape"],
+    train_dataset_size=cfg["train_dataset_size"]
+).to(device)
 
-if args.test_ood or "likelihood_ood_acc" in shared_cfg["test_metrics"]:
-    shared_evaluator = get_ood_evaluator(
-        two_step_module,
-        cfg=shared_cfg,
-        include_low_dim=True,
+
+if args.test_ood or "likelihood_ood_acc" in cfg["test_metrics"]:
+    evaluator = get_ood_evaluator(
+        module,
+        cfg=cfg,
+        include_low_dim=False,
         valid_loader=valid_loader,
         test_loader=test_loader,
         train_loader=train_loader,
         savedir=writer.logdir
     )
 else:
-    shared_evaluator = get_evaluator(
-        two_step_module,
+    if cfg["early_stopping_metric"] == "fid" and "fid" not in cfg["valid_metrics"]:
+        cfg["valid_metrics"].append("fid")
+
+    evaluator = get_evaluator(
+        module,
         train_loader=train_loader, valid_loader=valid_loader, test_loader=test_loader,
-        valid_metrics=shared_cfg["valid_metrics"],
-        test_metrics=shared_cfg["test_metrics"],
-        **shared_cfg.get("metric_kwargs", {}),
+        valid_metrics=cfg["valid_metrics"],
+        test_metrics=cfg["test_metrics"],
+        **cfg.get("metric_kwargs", {}),
     )
 
 
-trainer = get_trainer(
-    two_step_module=two_step_module,
+trainer = get_single_trainer(
+    module=module,
+    ckpt_prefix="gae" if cfg["gae"] else "de",
     writer=writer,
-    gae_cfg=gae_cfg,
-    de_cfg=de_cfg,
-    shared_cfg=shared_cfg,
+    cfg=cfg,
     train_loader=train_loader,
     valid_loader=valid_loader,
     test_loader=test_loader,
-    gae_evaluator=gae_evaluator,
-    de_evaluator=de_evaluator,
-    shared_evaluator=shared_evaluator,
-    load_best_valid_first=args.load_best_valid_first,
-    pretrained_gae_path=args.load_dir if args.load_pretrained_gae else "",
-    freeze_pretrained_gae=args.freeze_pretrained_gae if args.freeze_pretrained_gae else None,
+    evaluator=evaluator,
     only_test=args.only_test
 )
+
+checkpoint_load_list = ["latest", "best_valid"]
+if args.load_best_valid_first: checkpoint_load_list = checkpoint_load_list[::-1]
+for ckpt in checkpoint_load_list:
+    try:
+        trainer.load_checkpoint(ckpt)
+        break
+    except FileNotFoundError:
+        print(f"Did not find {ckpt} checkpoint")
+
 trainer.train()
